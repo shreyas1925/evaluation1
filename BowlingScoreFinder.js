d680438a111ad505fdeb91944a98a1e34356402b
Shreyas Shettigar <shreyasshettigar34@gmail.com>
2023-01-18 12:52:17 +0530

    feat: add function to calculate total score and best score

--- BowlingScoreFinder.js
+++ BowlingScoreFinder.js
@@ -1,66 +1,57 @@
 // let frames = 10
 
-let frames = [];
-let sum = 0;
+let frames = []
+let sum = 0
 
-let maxi = 0;
+let maxi = 0
 
 // if no strike had happened
 
 const getBestScore = (frames) => {
 
     frames.forEach(array => {
-        let runningSum = array[0] + array[1];
+        let runningSum = array[0] + array[1]
 
         if (runningSum > maxi) {
-            maxi = runningSum;
+            maxi = runningSum
         }
 
-    });
+    })
 
     return maxi;
 
-};
+}
 
 const getTotalScore = (rolls) => {
     for (let i = 0; i < rolls.length; i ++) {
 
-        if (rolls[i] !=10 && rolls[i+1] != 10) {
+        if (rolls[i] !=10) {
 
-            sum = rolls[i] + rolls[i+1];
-            let newarr = [];
-            newarr.push(rolls[i]);
-            newarr.push(rolls[i + 1]);
-            frames.push(newarr);
+            let newarr = []
+            newarr.push(rolls[i])
+            newarr.push(rolls[i + 1])
+            frames.push(newarr)
 
         }
-        else
+        if(rolls[i]==10)
         {
-            if (rolls[i] + rolls[i+1] == 10) {
-
-                sum = 10 + rolls[i+2];
-
-            }
-
             while (rolls[i] != 10) {
-                let newarr = [];
-                newarr.push(rolls[i]);
+                let newarr = []
+                newarr.push(rolls[i])
             }
         
         }
 
-        // sum += rolls[i] + rolls[i + 1];
+        sum += rolls[i] + rolls[i + 1];
     }
-    console.log(`Total score is ${sum}`);
-    return frames;
-};
+    console.log(`Total score is ${sum}`)
+    return frames
+}
 
-let rolls = [3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6];
+let rolls = [3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6]
 // let rolls = [ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 10, 10, 10]
 
-let resframes = getTotalScore(rolls);
-console.log(resframes);
-let bestScore = getBestScore(resframes);
-console.log(bestScore);
-
-module.exports = { getTotalScore, getBestScore}
+let resframes = getTotalScore(rolls)
+console.log(resframes)
+let bestScore = getBestScore(resframes)
+console.log(bestScore)
