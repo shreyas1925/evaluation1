0b92a36c6a31b8cfff209a317dbf93fe68e3abdf
Shreyas Shettigar <shreyasshettigar34@gmail.com>
2023-01-18 14:16:42 +0530

    feat: add function to calculate total score and best score

--- BowlingScoreFinder.js
+++ BowlingScoreFinder.js
@@ -38,7 +38,7 @@ const getTotalScore = (rolls) => {
 
         // basic test case 
         if(rolls[i] == 10){
-            sum+= + 10 + rolls[i+1] + rolls[i+2]
+            sum+= 10 + rolls[i+1] + rolls[i+2]
         }
 
         else if(rolls[i]+rolls[i+1] === 10){
