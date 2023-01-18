cf64252758276dac6a79877b286d2c98539e7039
Shreyas Shettigar <shreyasshettigar34@gmail.com>
2023-01-18 15:15:33 +0530

    test: add test for getTotalScore method

--- BowlingScoreFinder.test.js
+++ BowlingScoreFinder.test.js
@@ -1,12 +1,13 @@
 const {getTotalScore, getBestScore} = require("./BowlingScoreFinder")
 
 
-describe('Array utilities',()=>{
-    describe('double every number', () => { 
+describe('test cases for bowling game ',()=>{
+    describe('getTotalScore function to get total points in a game', () => { 
 
-        it('should double element when input is an array',()=>{
+        it('should return totalScore when input is an points array',()=>{
 
             expect(getTotalScore( [3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6])).toEqual(90);
         })
+
     })
 })
